f707074632555285dc61dd28c37c82484afd6ca0
Sushil <sushilgangavatiinventyv@gmail.com>
2023-12-13 11:41:25 +0530

    3 programs added

---
+++ FlowchartPrograms/PovNegFact.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class PovNegFact {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n = ");
+        int n = sc.nextInt();
+        int fact = 1, i;
+
+        for(i = 3;i <= (n*n/2);i+=2){
+            System.out.print(fact + " ");
+            fact = fact*i*(i-1)*(-1);
+        }
+    }
+}
